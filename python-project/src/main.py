ecfdc5def6192f3c001b95a060f3c28026ad0842
“wakai.ryo” <snake1115rw@yahoo.co.jp>
2026-02-23 16:29:37 +0900

    Update main.py: Change num to 100, modify output messages, and adjust loop range to 11

--- python-project/src/main.py
+++ python-project/src/main.py
@@ -1,12 +1,12 @@
 def main() -> None:
-    num=1;
+    num=100;
     num2=20;
     sum=sum_numbers(num, num2);
     if sum>10:
         print("Hello, World!")
-        print("Hello from python-project!")
+        print("Hello from python-project!!!")
         print("The sum", num, "and", num2, "is", sum)
-    for i in range(10):
+    for i in range(11):
         print(i)
 
 def sum_numbers(num, num2):
